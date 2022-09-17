7a5de4c229d27c234057b96e4ef6a2c660393f3a
Sean Davis <sdavisde@gmail.com>
2022-09-17 16:19:45 -0500

    added styles to game

--- components/AddVerse.js
+++ components/AddVerse.js
@@ -1,61 +1,72 @@
 import styles from '../styles/AddVerse.module.scss';
 import React, { useState, useEffect } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
-import { GetGroups, GetBooks, GetChapters, GetVerses, AddNewVerse } from '../middleware/verse';
+import { GetBooks, GetChapters, GetVerses, AddNewVerse } from '../middleware/verse';
 
 export default function AddVerse({ formSubmitted }) {
-    const [groups, setGroups] = useState([]);
     const [books, setBooks] = useState([]);
     const [bookId, setBookId] = useState(1);
     const [chapters, setChapters] = useState([]);
     const [verseList, setVerseList] = useState([]);
     const [verse, setVerse] = useState({
-        book: 'Genesis',
-        chapterId: 1,
-        verseId: 1,
+        book: '',
+        chapterId: null,
+        verseId: null,
         text: '',
         group: 'Group_2'
     });
+    const [chapterSelectDisabled, setChapterSelectDisabled] = useState(true);
+    const [verseSelectDisabled, setVerseSelectDisabled] = useState(true);
 
     useEffect(() => {
-        // GetGroups()
-        //     .then((data) => {
-        //         setGroups(data);
-        //         setVerse(prev => ({...prev, group: data[0].groupName ?? ''}));
-        //     });
-        
         GetBooks()
             .then((data) => {
                 setBooks(data);
             })
-    }, [])
+    }, []);
 
     let addVerse = (event) => {
-        console.log(verse);
         event.preventDefault();
-        AddNewVerse(verse)
-            .then((data) => {
-                formSubmitted();
-            });
+        if (verse.book != '' && verse.chapterId && verse.verseId && verse.text != '') {
+            AddNewVerse(verse)
+                .then((data) => {
+                    formSubmitted();
+                })
+        }
     };
 
     let setVerseText = (verseId) => {
+        if (!verseId) {
+            setVerse({
+                ...verse,
+                verseId: null,
+                text: ''
+            });
+            return;
+        }
+
         const target = verseList.find((v) => v.verseId == verseId);
-        setVerse(prev => ({...prev, verseId: target.verseId, text: target.verse }));
+        if (target) {
+            setVerse(prev => ({
+                ...prev,
+                verseId: target.verseId,
+                text: target.verse
+            }));
+        }
     }
 
-    let bookSelected = (id) => {
+    let bookSelected = (id, bookName) => {
         GetChapters(id).then((data) => {
             setChapters(data)
         });
-        setVerse(prev => ({...prev, chapterId: 1, verseId: 1 }));
+        setVerse(prev => ({...prev, book: bookName }));
     }
 
     let chapterSelected = (book_id, chapter_id) => {
         GetVerses(book_id, chapter_id).then((data) => {
             setVerseList(data)
         });
-        setVerse(prev => ({...prev, chapterId: chapter_id, verseId: 1 }));
+        setVerse(prev => ({...prev, chapterId: chapter_id }));
     }
 
     let onChange = (e) => {
@@ -65,20 +76,35 @@ export default function AddVerse({ formSubmitted }) {
         switch (e.target.name) {
             case 'book':
                 let book = books.find(book => book.name === e.target.value);
+                let bookName = e.target.value;
                 if (book) {
                     setBookId(book.id);
-                    bookSelected(book.id);
+                    bookSelected(book.id, bookName);
+                    setChapterSelectDisabled(false);
+                }
+                else {
+                    setChapterSelectDisabled(true);
+                    setVerseSelectDisabled(true);
                 }
                 break;
             case 'chapter':
                 let chapter = chapters.find(chapter => chapter.id === parseInt(e.target.value));
                 if (chapter) {
                     chapterSelected(bookId, e.target.value);
-                    break;
+                    setVerseSelectDisabled(false);
+                } else {
+                    setVerseSelectDisabled(true);
                 }
+                break;
             case 'verse':
-                let verseId = parseInt(e.target.value);
-                setVerseText(verseId);
+                if (e.target.value) {
+                    let verseId = parseInt(e.target.value);
+                    setVerseText(verseId);
+                }
+                else {
+                    setVerseText();
+                }
+                break;
         }
     };
 
@@ -92,13 +118,15 @@ export default function AddVerse({ formSubmitted }) {
                         <option key={key} id={book.id}>{book.name}</option>
                     )}
                 </datalist>
-                <select name="chapter" onChange={onChange} defaultValue={verse.chapter} className={styles.numList}>
+                <select name="chapter" onChange={onChange} defaultValue={verse.chapter} className={styles.numList} disabled={chapterSelectDisabled}>
+                    <option value=''>#</option>
                     {chapters.map((chapter, key) =>
                         <option key={key}>{chapter.id}</option>
                     )}
                 </select>
                 <span className={styles.colon}>:</span>
-                <select name="verse" onChange={onChange} defaultValue={verse.verse} className={styles.numList}>
+                <select name="verse" onChange={onChange} defaultValue={verse.verse} className={styles.numList} disabled={verseSelectDisabled}>
+                    <option value=''>#</option>
                     {verseList.map((verse, key) =>
                         <option key={key}>{verse.verseId}</option>
                     )}
