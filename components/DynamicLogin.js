77a6d52a2530b7c43e6a4e8dec3fe1295305f38f
Sean Davis <sdavisde@gmail.com>
2022-11-12 15:56:46 -0600

    Removing unused code

--- components/DynamicLogin.js
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const DynamicLoginWithNoSSR = dynamic(() => import('./Login'), {
-    ssr: false
-})
-
-export default function DynamicLogin ({ GlobalLogin, GlobalLogout }) {
-    return <DynamicLoginWithNoSSR GlobalLogin={GlobalLogin} GlobalLogout={GlobalLogout}/>
-} 
